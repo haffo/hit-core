33137a47ba3ac3b655cae68fec717cc1630dec4b
Affo, Harold <affoharold@yahoo.fr>
2015-09-18 13:47:04 -0400

    remove xml domain objects

--- hit-core-repo/src/main/java/gov/nist/hit/core/repo/XMLTestContextRepository.java
+++
@@ -1,9 +0,0 @@
-package gov.nist.hit.core.repo;
-
-import gov.nist.hit.core.domain.XMLTestContext;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface XMLTestContextRepository extends JpaRepository<XMLTestContext, Long> {
-
-}
