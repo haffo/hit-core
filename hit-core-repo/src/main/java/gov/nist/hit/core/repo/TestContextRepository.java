16631647937eadd6fdfbc5b778c476a58392c46a
Lefort, Maxence C <maxence.lefort@nist.gov>
2016-01-19 16:59:51 -0500

    Add query to retrieve a testContext from a messageId

--- hit-core-repo/src/main/java/gov/nist/hit/core/repo/TestContextRepository.java
+++ hit-core-repo/src/main/java/gov/nist/hit/core/repo/TestContextRepository.java
@@ -15,4 +15,7 @@ public interface TestContextRepository extends JpaRepository<TestContext, Long> 
   @Query("select tc.message from TestContext tc where tc.stage = :stage")
   public List<Message> findAllExampleMessages(@Param("stage") TestingStage stage);
 
+  @Query("select tc from TestContext tc where tc.message.id = :messageId")
+  public TestContext findOneByMessageId(@Param("messageId") Long messageId);
+  
 }
