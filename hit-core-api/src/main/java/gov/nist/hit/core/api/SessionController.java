2da0f939e8202c4a5e7f5ed8c68d621586601a23
Affo, Harold <affoharold@yahoo.fr>
2016-03-18 13:55:39 -0400

    added keep alive api

--- hit-core-api/src/main/java/gov/nist/hit/core/api/SessionController.java
+++ hit-core-api/src/main/java/gov/nist/hit/core/api/SessionController.java
@@ -12,39 +12,12 @@
 
 package gov.nist.hit.core.api;
 
-import gov.nist.hit.core.domain.AppInfo;
-import gov.nist.hit.core.domain.KeyValuePair;
-import gov.nist.hit.core.domain.Transaction;
-import gov.nist.hit.core.domain.TransportConfig;
-import gov.nist.hit.core.domain.User;
-import gov.nist.hit.core.repo.AppInfoRepository;
-import gov.nist.hit.core.repo.TransactionRepository;
-import gov.nist.hit.core.repo.TransportConfigRepository;
-import gov.nist.hit.core.repo.UserRepository;
-import gov.nist.hit.core.service.TransactionService;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.hibernate.annotations.Where;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.http.HttpRequest;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.data.jpa.domain.Specifications.where;
 
 /**
  * @author Harold Affo (NIST)
@@ -57,17 +30,23 @@ public class SessionController {
   @RequestMapping(value = "/delete", method = RequestMethod.POST)
   public boolean destroy(HttpServletRequest request) {
     HttpSession session = request.getSession(false);
-    if(session != null){
+    if (session != null) {
       session.invalidate();
     }
     return true;
-  } 
-  
+  }
+
   @RequestMapping(value = "/create", method = RequestMethod.POST)
   public boolean create(HttpServletRequest request) {
-    HttpSession session = request.getSession(true);   
+    HttpSession session = request.getSession(true);
     return true;
   }
-  
- 
+
+
+  @RequestMapping(method = RequestMethod.GET, value = "/keepAlive")
+  public boolean keepAlive(HttpServletRequest request) {
+    return true;
+  }
+
+
 }
