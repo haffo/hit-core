8fcfcf4ef8ef9d221954cd17e7f3a5b497376bcf
Affo, Harold <affoharold@yahoo.fr>
2016-04-01 13:04:35 -0400

    remvoe swagger unit test

--- hit-core-api/src/test/java/gov/nist/hit/core/api/unit/Swagger2MarkupTest.java
+++ hit-core-api/src/test/java/gov/nist/hit/core/api/unit/Swagger2MarkupTest.java
@@ -1,31 +1,20 @@
 package gov.nist.hit.core.api.unit;
 
-import gov.nist.hit.core.api.config.WebAppConfig;
 
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-@WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = WebAppConfig.class)
+// @WebAppConfiguration
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @ContextConfiguration(classes = WebAppConfig.class)
 public class Swagger2MarkupTest {
 
-  @Autowired
-  private WebApplicationContext context;
-
-  private MockMvc mockMvc;
-
-  @Before
-  public void setUp() {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-  }
+  // @Autowired
+  // private WebApplicationContext context;
+  //
+  // private MockMvc mockMvc;
+  //
+  // @Before
+  // public void setUp() {
+  // this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+  // }
 
   // @Test
   // public void convertSwaggerToAsciiDoc() throws Exception {
