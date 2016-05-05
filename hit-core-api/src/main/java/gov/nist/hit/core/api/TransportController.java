ec106ee366599648c01a0b03799d3b3ad95ce772
Affo, Harold <affoharold@yahoo.fr>
2016-05-05 16:07:04 -0400

    remove trManager

--- hit-core-api/src/main/java/gov/nist/hit/core/api/TransportController.java
+++ hit-core-api/src/main/java/gov/nist/hit/core/api/TransportController.java
@@ -34,7 +34,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -126,7 +125,6 @@ public class TransportController {
     return transactionService.findOne(transactionId);
   }
 
-  @Transactional(value = "transactionManager")
   @ApiOperation(value = "", nickname = "", hidden = true)
   @Cacheable(value = "HitCache", key = "'transport-forms'")
   @RequestMapping(value = "/config/forms", method = RequestMethod.GET)
